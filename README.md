1e89324360040a7e430a6922e339d9e019d8b578
j.yeo <j.yeo@260l038mac.members.cohub.uk>
2020-07-29 11:45:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+
+
+A very simple scripts to suggest a cidr blocks.
+
+Written to solve a minor problem of engineers deploying AWS VPCs with duplicate cidr blocks, and then VPC peering is required.
+
+The cidr blocks are 10.{x}.0.0/16. Where x is incremented until an unused cidr bock is found or 10.255.0.0/16 is hit. This may cause a problem in the future, but I think we'll have bigger problems if we've got 255 VPCs.
+
+# Running the script
+
+This script has much fewer moving parts as it builds a list of used cidr blocks using the AWS boto3 SDK
+
+```
+pip install -r requirements.txt
+```
+
+```
+python aws.py
+```
+
+## sample output
+
+```
+cidr_suggester git:(master) ✗ python main.py
+10.55.0.0/16
+```
---
+++ main.py
@@ -0,0 +1,68 @@
+import boto3
+import configparser
+import os
+
+
+def get_aws_profiles():
+    try:
+        path = os.path.join(os.path.expanduser("~"), ".aws/config")
+        parser = configparser.RawConfigParser()
+        parser.read(path)
+
+        profile_list = list()
+        for profile in parser.sections():
+            if "profile" in profile:
+                profile_list.append(profile.replace("profile ", ""))
+
+        return profile_list
+    except:
+        raise Exception(f"Error getting list of profiles from {path}")
+
+
+def get_cidr_blocks():
+    try:
+        c_blocks = list()
+        for aws_profile in get_aws_profiles():
+            sesssion = boto3.Session(profile_name=aws_profile, region_name="eu-west-1")
+            ec2_client = sesssion.client("ec2")
+
+            r = ec2_client.describe_vpcs()
+            for vpc in r["Vpcs"]:
+                for network in vpc["CidrBlockAssociationSet"]:
+                    c_blocks.append(network["CidrBlock"])
+        return c_blocks
+    except:
+        raise Exception("Error getting list of cidr blocks from aws")
+
+
+def create_range():
+    try:
+        cidr_notation = 16
+        starting_range = "10.0.0.0"
+        upper_limit = 255
+        starting_range_split = starting_range.split(".")
+        r_count = 0
+        range_used = True
+        while range_used == True or r_count == 255:
+
+            new_range = f"{starting_range_split[0]}.{r_count}.{starting_range_split[2]}.{starting_range_split[3]}/{cidr_notation}"
+
+            if new_range in cidr_blocks:
+                r_count += 1
+            elif r_count > upper_limit:
+                range_used = False
+                raise Exception(
+                    "I hoped this wouldn't ever happen, but we've reached the limits of this design"
+                )
+            else:
+                range_used = False
+                return new_range
+    except:
+        raise Exception("Error generating a new range")
+
+
+if __name__ == "__main__":
+    cidr_blocks = get_cidr_blocks()
+
+    new_range = create_range()
+    print(new_range)
